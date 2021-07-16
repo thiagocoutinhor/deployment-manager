b2efdb980a09c996fbc111722149e4cb44bbfcf2
Thiago Coutinho <thiagocoutinhor@gmail.com>
2021-07-16 17:29:55 -0300

    Scheduler

---
+++ Templates/scheduler/readme.md
@@ -0,0 +1,47 @@
+# Scheduler
+
+## Propriedades
+
+> Pelo menos um dentre `pubsub` ou `http` devem estar preenchidos
+
+- `name` Nome do schedule
+
+- `description` (opcional) Descrição do schedule
+
+- `cron` Agendamento no formato cron
+
+- `pubsub` Caso o alvo do schedule seja um pubsub
+
+- `topic` Tópico alvo no pubsub
+
+- `attributes` (opcional) Dicionário dos valores dos atributos
+
+- `http` Caso o alvo seja uma API (cloud functions são ativadas usando essa opção)
+
+- `method` Método HTTP a ser usado (POST, PUT, DELETE, etc.)
+
+- `url` Url a ser chamado
+
+- `serviceAccountEmail` (opcional) Email da conta de serviço a ser usado como autenticação
+
+- `payload` (opcional) Payload a ser enviado ao alvo. Deve estar encodado em base64
+
+- `region` (opcional) A região onde o schedule vai ser executado (padrão us-central1)
+
+- `labels` (opcional) Labels do schedule
+
+> Para ativar uma cloudfunction através de um shcedule, use a opção `http`
+
+## Payload e Base64
+
+Payloads para http só são permitidos se o `method` for POST, PUT ou PATCH. Qualquer outro method com um payload causará um erro.
+
+Para enviar um payload é necessário que ele seja [codificado em base64](https://www.base64encode.org/).
+
+## Código Python
+
+Para contornar a necessidade de encoding do payload (tanto em HTTP quando para Pub/Subs), o template em python faz esse trabalho e permite que o YAML enviado ao deployment manager tenha o texto do payload cru.
+
+## Outputs
+
+- `name` Nome recebido nas propriedades
---
+++ Templates/scheduler/scheduler.py
@@ -0,0 +1,46 @@
+from base64 import b64encode
+
+def generate_config(context):
+    resource = {
+        'name': f"sch-{context.properties['name']}",
+        'type': 'gcp-types/cloudscheduler-v1:projects.locations.jobs',
+        'properties': {
+            'parent': f"projects/{ context.env['project'] }/locations/{ context.properties.get('region', 'us-central1') }",
+            'labels': context.properties.get('labels'),
+            'description': context.properties.get('description'),
+            'schedule': context.properties['cron'],
+            'timeZone': 'America/Sao_Paulo'
+        }
+    }
+
+    # Se for HTTP
+    if 'http' in context.properties:
+        resource['properties']['http_target'] = {
+            'http_method': context.properties['http']['method'],
+            'uri': context.properties['http']['url']
+        }
+
+        if 'serviceAccountEmail' in context.properties['properties']['http']:
+            resource['properties']['http_target']['oidcToken'] = {
+                'serviceAccountEmail': context.properties['http']['serviceAccountEmail']
+            }
+
+        if 'payload' in context.properties:
+            resource['properties']['http_target']['body'] = b64encode(context.properties['payload'].encode())
+
+
+    # Se for pubsub
+    if 'pubsub' in context.properties:
+        resource['properties']['pubsubTarget'] = {
+            'topicName': f"projects/{ context.env['project'] }/topics/{ context.properties['pubsub']['topic'] }",
+            'data': context.properties.get('payload'),
+
+        }
+
+        if 'payload' in context.properties:
+            resource['properties']['pubsubTarget']['data'] = b64encode(context.properties['payload'].encode())
+
+        if 'attributes' in context.properties['pubsub']:
+            resource['properties']['pubsubTarget']['attributes'] = context.properties['pubsub']['attributes']
+
+    return { 'resources': [resource] }
